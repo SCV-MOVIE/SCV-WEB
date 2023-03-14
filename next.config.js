2c6341fbd0030bd195d77d49e4e0d71091422d94
Lee <43488305+poiu694@users.noreply.github.com>
2023-03-14 15:22:59 +0900

    feat: @svgr/webpack 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    config.resolve.fallback = { fs: false, path: false };
+
+    return config;
+  },
+};
 
 module.exports = nextConfig
