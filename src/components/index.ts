41386cba965073ddb8f93616a8d42efc9967fa79
Lee <43488305+poiu694@users.noreply.github.com>
2023-04-18 14:50:43 +0900

    feat: NumberInputBox 생성

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,4 +7,5 @@ export { default as MainLayout } from './MainLayout';
 export { default as LinedTitle } from './LinedTitle';
 export { default as MovieRating } from './MovieRating';
 export { default as ThumbnailCard } from './ThumbnailCard';
+export { default as NumberInputBox } from './NumberInputBox';
 export { default as MainMovieThumbnail } from './MainMovieThumbnail';
