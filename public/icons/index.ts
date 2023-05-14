1f972095a11cccd93dafa7458268bbe3b32a434a
LeeMir <manofgodmj@gmail.com>
2023-05-15 02:36:47 +0900

    chore: icon 추가

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -5,5 +5,25 @@ import Printer from './Printer.svg';
 import HalfStar from './HalfStar.svg';
 import Calendar from './Calendar.svg';
 import RefreshRight from './RefreshRight.svg';
+import DashBoard from './Dashboard.svg';
+import Users from './Users.svg';
+import CreditCard from './CreditCard.svg';
+import Timelapse from './Timelapse.svg';
+import LeftArrow from './LeftArrow.svg';
+import RightArrow from './RightArrow.svg';
 
-export { Star, Play, HalfStar, Calendar, Film, Printer, RefreshRight };
+export {
+  Star,
+  Play,
+  HalfStar,
+  Calendar,
+  Film,
+  Printer,
+  RefreshRight,
+  DashBoard,
+  Users,
+  CreditCard,
+  Timelapse,
+  LeftArrow,
+  RightArrow,
+};
